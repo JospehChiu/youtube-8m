3331f552a4037f20a2d77411a01bf61204ca128a
JospehChiu <b99501002@gmail.com>
2019-04-21 23:13:53 -0700

    Update eval.py

--- eval.py
+++ eval.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# Dummy
 """Binary for evaluating Tensorflow models on the YouTube-8M dataset."""
 
 import time
